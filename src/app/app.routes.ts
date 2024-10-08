c50c4b81210d41f20039e906dbf254e1db695506
sali1502 <sali1502@sudent.miun.se>
2024-10-08 11:49:31 +0200

    Undersidor, header och footer i components. Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CoursesComponent } from './pages/courses/courses.component';
+import { MyCoursesComponent } from './pages/my-courses/my-courses.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "pages/home", component: HomeComponent, title: "Startsidan" },
+    { path: "pages/courses", component: CoursesComponent, title: "Kurser" },
+    { path: "pages/my-courses", component: MyCoursesComponent, title: "Ramschema" },
+    { path: "", redirectTo: "pages/home", pathMatch: "full" },
+    { path: "404", component: NotFoundComponent },
+    { path: "**", component: NotFoundComponent }
+];
