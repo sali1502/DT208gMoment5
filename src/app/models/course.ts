9c8de1a78afbbd7d5a5a7dd8f63085e38af9cb7c
sali1502 <sali1502@sudent.miun.se>
2024-10-19 20:11:44 +0200

    Korrigering av kommentar för Interface

--- src/app/models/course.ts
+++ src/app/models/course.ts
@@ -1,6 +1,6 @@
 /* DT208G TypeScript, Moment 5 - Projekt. Åsa Lindskog sali1502@student.miun.se */
 
-// Interface för JSON-data som hämtas från api
+// Interface för JSON-data som hämtas från en fil
 export interface Course {
     courseCode: string,
     subjectCode: string,
